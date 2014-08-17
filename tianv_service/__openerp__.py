77aa86742fee5d26900786b14d2075120f26d6f3
cysnake4713 <cysnake4713@gmail.com>
2014-08-17 11:34:33 +0800

    [IMP]服务开发，基础数据

---
+++ tianv_service/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+# __author__ = cysnake4713@gmail.com
+# #############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Tianv Service Module',
+    'version': '0.1',
+    'author': 'cysnake4713',
+    'maintainer': 'odoosoft@gmail.com',
+    'website': 'http://www.odoosoft.com',
+    'description': u"""
+Tianv Service Module
+""",
+    'depends': ['base_override', 'tianv_sale'],
+    'category': 'Sales',
+    'demo_xml': [],
+    'data': [
+        'data/uom.xml',
+        # 'security/base_security.xml',
+        # 'security/ir.model.access.csv',
+        #
+        # 'views/partner_view.xml',
+        # 'views/partner_menu.xml',
+
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+}
