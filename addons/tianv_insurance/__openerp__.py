37d820170cd941f2e46eb703291d9187289b1285
cysnake4713 <cysnake4713@gmail.com>
2015-02-25 16:26:20 +0800

    [IMP]完成社保配置表单

--- addons/tianv_insurance/__openerp__.py
+++ addons/tianv_insurance/__openerp__.py
@@ -9,12 +9,12 @@ Tianv Social Insurance Module
 """,
     'author': 'Matt Cai',
     'website': 'http://odoosoft.com',
-    'depends': ['base', 'hr'],
+    'depends': ['base', 'hr', 'hr_contract'],
     'data': [
-        # 'security/security.xml',
+        'security/security.xml',
         'security/ir.model.access.csv',
         #
-        'views/insurance_view_type.xml',
+        'views/insurance_view.xml',
         'views/menu.xml',
     ],
     'qweb': [
--- addons/tianv_insurance/models/social_insurance.py
+++ addons/tianv_insurance/models/social_insurance.py
@@ -1,14 +1,55 @@
-__author__ = 'cysnak4713'
-
 # coding=utf-8
+__author__ = 'cysnak4713'
 from openerp import tools
 from openerp import models, fields, api
 from openerp.tools.translate import _
 
 
-class SocialInsurance(models.Model):
+class SocialInsuranceConfig(models.Model):
+    _name = 'tianv.social.insurance.config'
+    _rec_name = 'computer_code'
+
+    contract = fields.Many2one('hr.contract', 'Hr Contract')
+    computer_code = fields.Char('Computer Code')
+    census_type = fields.Selection([('city', u'市内城镇'), ('country', u'市外农村')], 'Census Type')
+    lines = fields.One2many('tianv.social.insurance.line', 'config', 'Config Lines')
+
+    personal_total = fields.Float('Personal Total', (10, 2), compute='_compute_total')
+    company_total = fields.Float('Company Part', (10, 2), compute='_compute_total')
+    total = fields.Float('Total', (10, 2), compute='_compute_total')
+
+    active = fields.Boolean('Is Active')
+
+    @api.multi
+    def _compute_total(self):
+        for config in self:
+            personal_total = 0
+            company_total = 0
+            for line in config.lines:
+                personal_total += line.personal_part
+                company_total += line.company_part
+            config.personal_total = personal_total
+            config.company_total = company_total
+            config.total = config.personal_total + config.company_total
+
+    _defaults = {
+        'active': True,
+    }
+
+
+class SocialInsuranceType(models.Model):
     _name = 'tianv.social.insurance.type'
 
     name = fields.Char('Name', required=True)
 
     _sql_constraints = [('social_insurance_type_name_unique', 'unique(name)', _('name must be unique !'))]
+
+
+class SocialInsuranceLine(models.Model):
+    _name = 'tianv.social.insurance.line'
+    _rec_name = 'type'
+
+    type = fields.Many2one('tianv.social.insurance.type', 'Insurance Type', required=True)
+    company_part = fields.Float('Company Part', (10, 2))
+    personal_part = fields.Float('Personal Part', (10, 2))
+    config = fields.Many2one('tianv.social.insurance.config', 'Related Config')
