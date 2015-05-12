f91b8b118d7e92988b01cf15eabd540fceac1f6f
cysnake4713 <cysnake4713@gmail.com>
2015-05-12 16:54:35 +0800

    [FIX]tianv_hr:添加基础信息字段

--- addons/tianv_hr/models/hr.py
+++ addons/tianv_hr/models/hr.py
@@ -11,3 +11,15 @@ class EmployeeInherit(models.Model):
 
     residence_address = fields.Char('Residence Address')
 
+    nationality = fields.Char('Nationality')
+    school = fields.Char('School')
+    educational = fields.Char('Educational Background')
+    major = fields.Char('Major')
+    language_skill = fields.Char('Language Skill')
+    basic_english = fields.Char('Basic English')
+    qq_id = fields.Char('QQ')
+    wechat_id = fields.Char('Wechat')
+    person_mail = fields.Char('Personal Mail')
+    family_members = fields.Char('Family Members')
+
+    attachments = fields.Many2many('ir.attachment', 'employee_attachment_rel', 'employee_id', 'attachment_id', 'Attachments')
