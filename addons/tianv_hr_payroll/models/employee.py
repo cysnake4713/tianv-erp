0df908db9e77655ade4a1b733fcb46978ca5e795
cysnake4713 <cysnake4713@gmail.com>
2015-05-25 20:16:44 +0800

    [FIX]tianv_hr_payroll:修复获取社保错误

--- addons/tianv_hr_payroll/models/employee.py
+++ addons/tianv_hr_payroll/models/employee.py
@@ -1,7 +1,7 @@
 __author__ = 'cysnake4713'
 
 # coding=utf-8
-from openerp import tools
+from openerp import tools, exceptions
 from openerp import models, fields, api
 from openerp.tools.translate import _
 from dateutil import rrule
@@ -23,7 +23,8 @@ class HrEmployee(models.Model):
     def get_insurance_record_by_date(self, date_start, date_end):
         try:
             return self.env['tianv.social.insurance.record'].search(
-                [('period.date_start', '<=', date_start), ('period.date_stop', '>=', date_end), ('state', '=', 'confirm')]).ensure_one()
+                [('period.date_start', '<=', date_start), ('period.date_stop', '>=', date_end), ('state', '=', 'confirm'),
+                 ('employee', '=', self.id)]).ensure_one()
         except Exception:
             raise exceptions.Warning(_("can't find insurance record in period or have multi record for one period"))
 
