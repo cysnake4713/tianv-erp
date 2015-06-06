c5c2a0f08a706d43a26d78fad84981f5a75b1c3b
cysnake4713 <cysnake4713@gmail.com>
2015-06-06 23:22:00 +0800

    [FIX]tianv_hr_payroll:打印修复

--- addons/tianv_hr_payroll/models/payslip.py
+++ addons/tianv_hr_payroll/models/payslip.py
@@ -11,6 +11,7 @@ from openerp.tools.safe_eval import safe_eval as eval
 class HrPayslip(osv.Model):
     _name = 'hr.payslip'
     _inherit = ['hr.payslip', 'odoosoft.workflow.abstract']
+    _order = 'date_from'
 
     _columns = {
         'state': fields.selection([
