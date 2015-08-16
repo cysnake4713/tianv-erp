0e0237a346befa913703d57324960065f2dbb9d9
cysnake4713 <cysnake4713@gmail.com>
2015-08-16 17:24:19 +0800

    [UPD]tianv_account:添加凭证权限给发票仔

---
+++ addons/tianv_account/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# __author__ = cysnake4713@gmail.com
+{
+    'name': 'Tianv Account Module',
+    'version': '0.2',
+    'category': 'tianv',
+    'complexity': "easy",
+    'description': """
+Tianv Account""",
+    'author': 'Matt Cai',
+    'website': 'http://odoosoft.com',
+    'depends': ['base'],
+    'data': [
+        'views/menuitem.xml',
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'demo': [],
+    'application': True
+}
