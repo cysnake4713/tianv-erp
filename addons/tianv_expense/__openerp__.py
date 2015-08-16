77f345cd2f4a88d852578924a9e21009054f7a06
cysnake4713 <cysnake4713@gmail.com>
2015-08-16 17:37:34 +0800

    [UPD]tianv_expense:添加费用功能

---
+++ addons/tianv_expense/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# __author__ = cysnake4713@gmail.com
+{
+    'name': 'Tianv Expense Module',
+    'version': '0.2',
+    'category': 'tianv',
+    'complexity': "easy",
+    'description': """
+Tianv Expense""",
+    'author': 'Matt Cai',
+    'website': 'http://odoosoft.com',
+    'depends': ['base', 'hr_expense'],
+    'data': [
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'demo': [],
+    'application': True
+}
